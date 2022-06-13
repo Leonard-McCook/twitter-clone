36ad3e87a0da6b83f388c401a621b87caddce5a7
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-06-13 15:38:03 -0400

    Add components folder

---
+++ components/Sidebar.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+export default function Sidebar() {
+  return (
+    <div>
+        {/* Twitter Logo */}
+        <div className="">
+            <Image width="50" height="50" src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"></Image>
+        </div>
+
+
+        {/* Menu */}
+
+
+        {/* Button */}
+
+        {/* Mini-Profile */}
+
+    </div>
+  )
+}
