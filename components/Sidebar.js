054dfd03bb12551707a1d27076ecf3e6ed9a35ca
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-06-15 15:37:16 -0400

    Add Sidebar menu icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import SidebarMenuItem from './SidebarMenuItem';
 import {HomeIcon} from "@heroicons/react/solid"
+import {BellIcon, BookmarkIcon, ClipboardIcon, HashtagIcon, InboxIcon, UserIcon, DotsCircleHorizontalIcon} from "@heroicons/react/outline"
+
 export default function Sidebar() {
   return (
     <div>
-        {/* Twitter Logo */}
+      {/* Twitter Logo */}
         <div className="">
             <Image 
               width="50"
@@ -19,6 +21,13 @@ export default function Sidebar() {
         {/* Menu */}
         <div className="">
           <SidebarMenuItem text="Home" Icon={HomeIcon} />
+          <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+          <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+          <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+          <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+          <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+          <SidebarMenuItem text="Profile" Icon={UserIcon} />
+          <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
         </div>
 
 
