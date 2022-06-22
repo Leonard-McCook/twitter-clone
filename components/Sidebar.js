af1fd811964bf19f3001a5252827249f12550d43
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-06-22 09:48:08 -0400

    Add margin to sidebar icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -19,7 +19,7 @@ export default function Sidebar() {
 
 
         {/* Menu */}
-        <div className="">
+        <div className="mt-4 mb-2.5 xl:items-start">
           <SidebarMenuItem text="Home" Icon={HomeIcon} active />
           <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
           <SidebarMenuItem text="Notifications" Icon={BellIcon} />
