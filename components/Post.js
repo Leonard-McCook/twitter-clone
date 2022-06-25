8df3b4cab3bff6f7ab57d695aa3768e181610754
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-06-25 18:56:56 -0400

    Add user image/name to post

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,41 @@
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
+
 export default function Posts({post}) {
-  return (
-    <div>{post.id}</div>
-  )
+  return <div>
+        {/* user image */}
+        <img src={post.userImg} alt="user image"  />
+
+        {/* right side */}
+        <div className="">
+        {/* Header */}
+
+          <div className="">
+              {/* post user info */}
+              <div className="">
+                <h4>{post.name}</h4>
+                 <span>{post.username}</span>
+                 <span>{post.timestamp}</span>
+                
+
+              </div>
+
+
+              {/* dot icon */}
+              <DotsHorizontalIcon className="h-10"/>
+          </div>
+
+          {/* post text */}
+
+          {/* post image */}
+
+          {/* icons */}
+
+          <div className="">
+
+            
+          </div>
+
+
+        </div>
+  </div>
 }
