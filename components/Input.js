f5474a9bfc02820cb996338401b577edc3a35f6b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-07-03 02:56:10 -0400

    Setup database for tweets

--- components/Input.js
+++ components/Input.js
@@ -1,9 +1,25 @@
 import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react"
-
+import { useState } from "react";
+import { db } from "../firebase";
 export default function Input() {
     const {data: session} = useSession();
-    console.log(session);
+    const [input, setInput] = useState("");
+
+    const sendPost = async ()=> {
+        const docRef = await addDoc(collection(db, "posts"), {
+            id: session.user.uid,
+            text: input,
+            userImg: session.user.image,
+            timestamp: serverTimestamp(),
+            name: session.user.name,
+            username: session.user.username,
+        });
+
+        setInput("");
+    };
+
 ;   return (
         <>
         {session && (
@@ -15,14 +31,19 @@ export default function Input() {
                  className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"/>
                 <div className="w-full divide-y divide-gray-200">
                     <div className="">
-                        <textarea className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?"></textarea>
+                        <textarea 
+                        className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?" 
+                        value={input} 
+                        onChange={(e)=>setInput(e.target.value)}>
+
+                        </textarea>
                     </div>
                     <div className="flex items-center justify-between pt-2.5">
                         <div className="flex">
                             <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
                             <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
                         </div>
-                        <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
+                        <button onClick={sendPost} disabled={!input.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
                     </div>
                 </div> 
             </div>
