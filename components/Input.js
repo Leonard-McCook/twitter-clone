a64ee05f57d34bb42a6d09056c10ab3b633ba87b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-07-03 13:33:04 -0400

    Add function for uploading images

--- components/Input.js
+++ components/Input.js
@@ -1,13 +1,16 @@
 import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { useSession, signOut } from "next-auth/react"
-import { useState } from "react";
-import { db } from "../firebase";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { useSession, signOut } from "next-auth/react";
+import { useState, useRef } from "react";
+import { db, storage } from "../firebase";
 export default function Input() {
     const {data: session} = useSession();
     const [input, setInput] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+    const filePickerRef = useRef(null);
 
-    const sendPost = async ()=> {
+    const sendPost = async () => {
         const docRef = await addDoc(collection(db, "posts"), {
             id: session.user.uid,
             text: input,
@@ -17,7 +20,31 @@ export default function Input() {
             username: session.user.username,
         });
 
+        const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+        if (selectedFile) {
+            await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+                const downloadURL = await getDownloadURL(imageRef);
+                await updateDoc(doc(db, "posts", docRef.id), {
+                    image: downloadURL,
+                });
+             });
+        }
+
         setInput("");
+        
+    };
+
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if(e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result);
+        };
+
     };
 
 ;   return (
@@ -32,7 +59,7 @@ export default function Input() {
                 <div className="w-full divide-y divide-gray-200">
                     <div className="">
                         <textarea 
-                        className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?" 
+                        className="w-full border-none focus:ring-0 text-lg placeholder-gray-500 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?" 
                         value={input} 
                         onChange={(e)=>setInput(e.target.value)}>
 
@@ -40,7 +67,13 @@ export default function Input() {
                     </div>
                     <div className="flex items-center justify-between pt-2.5">
                         <div className="flex">
-                            <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
+                            <div className="" onClick={() => filePickerRef.current.click()}>
+                                <PhotographIcon className="h-10 w-10 hoverEffect p-2     text-sky-500 hover:bg-sky-100"/>
+                                <input type="file" 
+                                  hidden 
+                                  ref={filePickerRef} 
+                                  onClick={addImageToPost} />
+                            </div>
                             <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
                         </div>
                         <button onClick={sendPost} disabled={!input.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
@@ -49,5 +82,5 @@ export default function Input() {
             </div>
             )}
         </>
-  );
-}
+    );
+ }
